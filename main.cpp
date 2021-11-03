10826ae32227662084e9d4d215c1bd5f1b8a7526
alice-gdt <goudout.alice@gmail.com>
2021-11-03 19:41:34 +0100

    [MAIN] Features matching with FLANN

--- main.cpp
+++ main.cpp
@@ -10,6 +10,12 @@
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/video.hpp>
+#include "opencv2/core.hpp"
+#include "opencv2/features2d.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/xfeatures2d.hpp"
+
+using namespace cv::xfeatures2d;
 
 #include <vector>
 #include <dirent.h>
@@ -21,121 +27,23 @@ using std::endl; using std::vector;
 using namespace cv;
 using namespace std;
 
-//Yes it's hardcoded, but at least it works
-//std::list <int**> lib_assets;
-int lib_assets[6][6][6] = {
-			{{1, 0, 0, 1, 0, 0},
-			{1, 0, 0, 1, 1, 1},
-			{1, 1, 0, 1, 1, 0},
-			{0, 1, 0, 1, 0, 1},
-			{0, 0, 1, 0, 0, 0},
-			{0, 1, 0, 1, 0, 0}},
-
-			{{1, 0, 1, 0, 0, 0},
-			{0, 1, 1, 0, 1, 0},
-			{1, 1, 1, 0, 0, 0},
-			{0, 0, 0, 1, 1, 0},
-			{0, 0, 1, 0, 1, 0},
-			{1, 1, 1, 0, 0, 0}},
-
-			{{1, 1, 0, 0, 0, 0},
-			{1, 0, 1, 0, 1, 0},
-			{1, 0, 0, 1, 0, 0},
-			{1, 0, 1, 0, 0, 0},
-			{1, 0, 1, 0, 0, 0},
-			{1, 0, 0, 1, 0, 0}},
-
-			{{1, 0, 0, 1, 1, 1},
-			{0, 1, 0, 0, 1, 0},
-			{1, 0, 0, 0, 1, 0},
-			{1, 0, 1, 1, 0, 1},
-			{1, 1, 0, 1, 0, 0},
-			{0, 1, 0, 0, 0, 0}},
-
-			{{1, 1, 0, 1, 1, 1},
-			{0, 0, 1, 0, 0, 0},
-			{0, 1, 0, 0, 1, 0},
-			{0, 0, 0, 1, 0, 0},
-			{0, 0, 0, 1, 0, 0},
-			{1, 1, 1, 0, 0, 0}},
-
-			{{1, 0, 0, 0, 1, 0},
-			{0, 1, 0, 1, 0, 1},
-			{0, 0, 1, 1, 0, 0},
-			{0, 1, 0, 1, 0, 0},
-			{0, 1, 1, 0, 1, 0},
-			{1, 0, 0, 0, 0, 1}},
-
-			};
-
-void print_debug(int matrix[6][6])
-{
-	for (int i = 0; i < 6; i++)
-	{
-		for (int j = 0; j < 6; j++)
-		{
-			std::cout << matrix[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}
-}
-
-void load_lib(char *dir_path)
-{
-	int mat[6][6] = {0};
-
-	DIR *dir; struct dirent *diread;
-
-	if ((dir = opendir(dir_path)) != nullptr) {
-		while ((diread = readdir(dir)) != nullptr) {
-			char *name = diread->d_name;
-			if (strstr(name, "asset")) {
-  				char tmp[256];
-      			getcwd(tmp, 256);
-				name = strcat(strcat(strcat(tmp, "/"), dir_path), name);
-				Mat image = imread(name);
-				if (image.empty()) {
-   					std::cout << "Could not open or find the image";
-		  			break;
- 				}
-				else {
-		  			//TODO traitement de image pour remplir mat
-		  			//print la mat remplie ?
- 		  			String windowName = "Asset";
- 		  			namedWindow(windowName);
-        			imshow(windowName, image);
-	 	  			waitKey(0);
-        			destroyWindow(windowName);
-				}
-	  		}
-		}
-  		closedir (dir);
-  	}
-  	else {
-    	perror (dir_path);
-  	}
-}
-
 int main(int argc, char **argv) {
-	if (argc != 2 && argc != 3) {
-    	cout << "Veuillez passer une vidéo en 2e argument."  << endl;
+	if (argc != 3 && argc != 4) {
+    	cout << "Input should be in the following format :"  << endl;
+    	cout << "./tech_test_JS <path>/*.mp4 <path to assets> [IM]"  << endl;
     	return -1;
-  	}
-  	bool option = false;
-  	if (argc == 3) {
-    	string str = argv[2];
-    	if (str.compare("IM") == 0)
-      		option = true;
-		else if (str.compare("load") == 0)
-		{
-			//load_lib();
-			cout << "TODO : load_lib()" << endl;
+	}
+	bool option = false;
+	if (argc == 4) {
+		string str = argv[3];
+		if (str.compare("IM") == 0)
+			option = true;
+		else {
+			cout << "Input should be in the following format :"  << endl;
+			cout << "./tech_test_JS <path>/*.mp4 <path to assets> IM"  << endl;
+			return -1;
 		}
-    	else {
-      		cout << "La seule option valide est IM ou load"  << endl;
-      		return -1;
-    	}
-  	}
+	}
 
 	VideoCapture cap(argv[1]); 
 
@@ -145,59 +53,114 @@ int main(int argc, char **argv) {
 	}
 
 	Mat frame;
-	Mat imgGRAY, thresh;
+	Mat imgGRAY;
 	frame.convertTo(frame, CV_32F);
 	imgGRAY.convertTo(imgGRAY, CV_32F);
-	thresh.convertTo(thresh, CV_32F);
+
+	Mat asset;
+
+	int minHessian = 400;
+
+	Ptr<SURF> detector = SURF::create();
+	detector->setHessianThreshold(minHessian);
+
+	std::vector<KeyPoint> keypoints_1, keypoints_2;
+	Mat descriptors_1, descriptors_2;
+
 
 	while(1) {
 		cap >> frame;
 		if (frame.empty())
 			break;
 
-		//frame.astype(np.float32);
 		cvtColor(frame, imgGRAY, COLOR_BGR2GRAY);
-	
-		inRange(imgGRAY, Scalar(175, 175, 175), Scalar(255, 255, 255), thresh);
-      
-		erode(thresh, thresh, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-		dilate(thresh, thresh, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
 
-		dilate(thresh, thresh, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
-		erode(thresh, thresh, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+		DIR *dir; struct dirent *diread;
+		double max_d = 0.0;
+		double min_d = 1.0;
+		int good_match = 0;
+		char *asset_name = (char *) "No asset";
+		double delta = 0.005;
 
-		vector<vector<Point> > contours;
-		vector<Vec4i> hierarchy;
+		if ((dir = opendir(argv[2])) != nullptr) {
+			while ((diread = readdir(dir)) != nullptr) {
+				char *name = diread->d_name;
+				char *save_name = diread->d_name;
+				if (strstr(name, "asset")) {
+					char tmp[256];
+      		getcwd(tmp, 256);
+					name = strcat(strcat(strcat(tmp, "/"), argv[2]), name);
+					asset = imread(name);
+					if (asset.empty()) {
+   						std::cout << "Could not open or find the image";
+		  				break;
+ 					}
+					else {
+						detector->detectAndCompute( imgGRAY, Mat(), keypoints_1, descriptors_1 );
+						detector->detectAndCompute( asset, Mat(), keypoints_2, descriptors_2 );
 
-		findContours(thresh, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE );
+						//-- Step 2: Matching descriptor vectors using FLANN matcher
+						FlannBasedMatcher matcher;
+						std::vector< DMatch > matches;
+						matcher.match( descriptors_1, descriptors_2, matches );
 
-		Mat dst = Mat::zeros(frame.rows, frame.cols, CV_8UC3);
-		dst.convertTo(dst, CV_32F);
+						double max_dist = 0; double min_dist = 100;
 
-		if (hierarchy.size() != 0) {
-			//vector<Point> red;
-			
-			std::vector<cv::Point> approx;
+						//-- Quick calculation of max and min distances between keypoints
+						for( int i = 0; i < descriptors_1.rows; i++ ) {
+							double dist = matches[i].distance;
+							if( dist < min_dist ) min_dist = dist;
+							if( dist > max_dist ) max_dist = dist;
+						}
 
-			for (int i = 0; i < hierarchy.size(); i++) {
-				//approxPolyDP(cv::Mat(hierarchy[i]), approx, cv::arcLength(cv::Mat(hierarchy[i]), true)*0.02, true);
-				//result = cvApproxPoly(*threshR, sizeof(CvContour), contoursR, CV_POLY_APPROX_DP, cvContourPerimeter(&hierarchyR)*0.02, 0);
+						cout << name << endl;
+						cout << "-- Max dist : " << max_dist << endl;
+						cout << "-- Min dist : " << min_dist << endl;
 
-				//if (approx.size() > 4)
-					//Scalar color(0, 0, 255);
-				drawContours(dst, contours, i, Scalar(0, 0, 255), 2, 8, hierarchy, 0, Point() );
-				//else
-					//Scalar color(0, 255, 0);
-					//drawContours(dst, contoursR, i, Scalar(0, 255, 0), 2, 8, hierarchyR, 0, Point() );
+						//-- Draw only "good" matches (i.e. whose distance is less than 2*min_dist,
+						//-- or a small arbitary value ( 0.02 ) in the event that min_dist is very
+						//-- small)
+						//-- PS.- radiusMatch can also be used here.
+						std::vector< DMatch > good_matches;
+
+						for( int i = 0; i < descriptors_1.rows; i++ ) {
+							if( matches[i].distance <= max(2*min_dist, 0.02) ) {
+								good_matches.push_back( matches[i]);
+							}
+						}
+
+						//-- Draw only "good" matches
+						Mat img_matches;
+						drawMatches(imgGRAY, keypoints_1, asset, keypoints_2,
+												good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+												vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+
+						//-- Show detected matches
+						imshow( "Good Matches", img_matches );
+
+						cout << "-- Good Match " << (int) good_matches.size() << endl;
+
+						if (max_dist - max_d > delta 	&&
+								min_dist - min_d < delta	&&
+								good_match < (int) good_matches.size()) {
+							max_d = max_dist;
+							min_d = min_dist;
+							good_match = (int) good_matches.size();
+							asset_name = save_name;
+							cout << "MAX_D : " << max_d << " MIN_D : " << min_d << " MATCHES : " << good_match << " NAME : " << asset_name  << endl;
+						}
+					}
+				}
 			}
-		}
- 
-		//imshow("Original", frame);
-		//imshow("Gray", imgGRAY);
-		imshow("Black & White", thresh);
+  			closedir (dir);
+  		}
+  		else {
+    		perror (argv[2]);
+  		}
+
+			cout << "Our winner is " << asset_name << endl;
 
 		if (option) {
-			imshow("Contours détectés", dst);
 			waitKey(0);
 		}
 		else {
